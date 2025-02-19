35e76604134bb6e7ba5e6db83bea0ecaa57574bb
Edouard <edouardschneiders@gmail.com>
2025-02-19 17:39:35 +0100

    test

--- admin-ui/src/api/price/PriceWhereInput.ts
+++ admin-ui/src/api/price/PriceWhereInput.ts
@@ -1,5 +1,14 @@
 import { StringFilter } from "../../util/StringFilter";
+import { FloatFilter } from "../../util/FloatFilter";
+import { DateTimeFilter } from "../../util/DateTimeFilter";
 
 export type PriceWhereInput = {
   id?: StringFilter;
+  amount?: FloatFilter;
+  currency?: StringFilter;
+  createdAt?: DateTimeFilter;
+  updatedAt?: DateTimeFilter;
+  description?: StringFilter;
+  isActive?: boolean;
 };
+
